344e810391a177dc672d6996e5bc11824782604a
Rolando Huitrón Acosta <pochock.23@gmail.com>
2020-12-15 14:13:23 -0600

    func-huboerror_actualizarCovid

--- AppCovid19/CovidManager.swift
+++ AppCovid19/CovidManager.swift
@@ -8,6 +8,15 @@
 
 import Foundation
 
+protocol CovidManagerDelegate {
+    func actualizarCovid(covid: CovidModelo)
+    
+    func huboError(cualError: Error)
+    
+}
+
+
+
 struct CovidManager {
     
     let covidURL = "https://corona.lmao.ninja/v3/covid-19/countries"
@@ -16,5 +25,44 @@ struct CovidManager {
         let urlString = "\(covidURL)/\(nombrePais)"
         print(urlString)
     }
+    
+    func realizarSolicitud(urlString: String) {
+        
+        if let url = URL(string: urlString){
+            
+            let session = URLSession(configuration: .default)
+            
+            let tarea = session.dataTask(with: url) { (data, respuesta, error) in if error != nil {
+                
+                self.delegado?.Hubo
+                
+                return
+                
+                }
+                
+            }
+        }
+        
+    }
+    
+    
+    
+    func parseJSON(covidData: Data) -> CovidModelo{
+        let decoder = JSONDecoder()
+        
+        do{
+            let dataDecodificada = try decoder.decode(covidData.self, from: covidData)
+            
+            
+            
+            
+        }catch{
+            
+        }
+        
+        
+        
+    }
+    
     
 }
--- AppCovid19/ViewController.swift
+++ AppCovid19/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var covidManager = CovidManager()
+    
     
     @IBOutlet weak var buscarPais: UITextField!
     
@@ -29,13 +31,67 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        covidManager.delegado = self
+        
+        buscarPais.delegate = self
+        
+        
     }
     
     
     @IBAction func BuscarPaises(_ sender: UIButton) {
     }
+
+}
+
+// MARK: - Hacer la peticion a la API
+extension ViewController: CovidManagerDelegate {
     
+    func huboError(cualError: Error){
+        
+        DispatchQueue.main.async {
+            self.nombrePaislabel.text = cualError.localizedDescription
+        }
+        
+        print(cualError.localizedDescription)
+        
+    }
+    
+    func actualizarCovid(covid: CovidModelo){
+        
+        DispatchQueue.main.async {
+            
+            self.nombrePaislabel.text = covid.nombrePais
+            self.totalCasoslabel.text = "Total de casos: \(String(covid.totalCasos))"
+            self.totalRecuperadoslabel.text = "Total de recuperados: \(String(covid.totalRecuperados))"
+            self.totalMuertoslabel.text = "Total de muertos: \(String(covid.totalMuertos))"
+            
+        }
+    }
+}
 
-
+//MARK: - delegado del TextField
+extension ViewController : UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        buscarPais.text = ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        covidManager.fetchCovid(nombrePais: buscarPais.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if buscarPais.text != ""{
+            return true
+        } else {
+            buscarPais.placeholder = "escribe una ciudad"
+            print("Por favor escribe algo..")
+            return false
+        }
+    }
+    
+    
 }
 
